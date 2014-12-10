2ea9d0c160adbdb8be170a02197951042bbe3d32
bcawthorne7296 <bcawthorne7296@sohamcollege.org.uk>
2014-12-10 09:45:00 +0000

    "Improved Commmenting"

--- Pi Camera/Guess who?/take_picture.py
+++ Pi Camera/Guess who?/take_picture.py
@@ -1,47 +1,46 @@
 import picamera,time #imports modules of the picamera and time
 
-def getPicture(name="Error"):
+def getPicture(name="Error"): #defines function for getting picture. If they don't provide a name, the picture will be saved as 'error'
     try:
         check = False
         while check == False:
             with picamera.PiCamera() as camera: #does the softwre inside the loop then deletes it
                 camera.resolution = (1024,768) #sets camera resolution
-                camera.start_preview() 
+                camera.start_preview() #displays picture preview
                 time.sleep(2) #pauses for 2 seconds
                 filename = "{0}.jpeg".format(name)
                 camera.capture(filename) #takes picture and calls it pictures.jpeg
-                camera.stop_preview()
+                camera.stop_preview() #stops showing the preview
                 response = input("Are you happy with your picture(Y/N)")
-            if response == "Y":
+            if response.lower() == "y": #If they re happy with the picture, the program will end. If not, it'll repeat
                 check=True
-            elif response == "y":
-                check=True
-    except picamera.exc.PiCameraMMALError:
+    
+    except picamera.exc.PiCameraMMALError: #if the camera isn't connected, it will tell the user instead of sending out an error
         print("There is an error with the camera. Please check it is connected correctly")
         filename = ""
-    except picamera.exc.PiCameraError:
+    except picamera.exc.PiCameraError:#if the user hasn't stated the right parameters, itr will get the user to try again
          print("Please ensure you have stated your name in speech marks in the brackets.")
          filename = ""
     return filename
 
-def getCharProfile():
+def getCharProfile():#defines a function for collecting someone's characteristics
     name = ""
-    while name == "":
+    while name == "":#the loops will repeat until the person enters something
         name = input("What is your name?")
-        if input("Is your name " + name + "?(Y/N)").lower() == "y":
-            getPicture(name)
+        if input("Is your name " + name + "?(Y/N)").lower() == "y": #checks if the name given is correct
+            getPicture(name) #takes picture and saves it with the name of the person that they provided
         else:
             name=""
     hairResponse = ""
-    while hairResponse=="":
+    while hairResponse=="": 
         hairResponse = input("What colour is your hair?")
     hatResponse = ""
     while hatResponse=="":
         hatResponse = input("Are you wearing a hat(Y/N)?")
         if hatResponse.lower() == "y":
-            hatResponse = True
+            hatResponse = True#if the person is wearing a hat, it sets hat to true
         else:
-            hatResponse = False
+            hatResponse = False #if the person isn't wearing a hat, it sets hat to false
     eyeResponse = ""
     while eyeResponse=="":
         eyeResponse = input("What colour are your eyes?")
@@ -62,7 +61,7 @@ def getCharProfile():
             glassesResponse = True
         else:
             glassesResponse = False
-    return [name,hairResponse,hatResponse,eyeResponse,genderResponse,facialResponse,glassesResponse]
+    return [name,hairResponse,hatResponse,eyeResponse,genderResponse,facialResponse,glassesResponse]#returns all the persons characteristics
     
     
     
