9be56d0da28cdb6bac030dcc91d1d845548055e5
bcawthorne7296 <bcawthorne7296@gmail.com>
2014-12-12 13:43:28 +0000

    Update Timelapse.py

--- Pi Camera/Timelapse/Timelapse.py
+++ Pi Camera/Timelapse/Timelapse.py
@@ -1,9 +1,9 @@
-import time, picamera
+import time, picamera #imports the modules for the picamera and time
 
 with picamera.PiCamera() as camera:
-    camera.start_preview()
-    time.sleep(2)
+    camera.start_preview() #starts preview of the picture it will take
+    time.sleep(2) #pauses program for two seconds
     for filename in camera.capture_continuous('img{counter:03d}.jpg'):
         print('Captured %s' % filename)
-        time.sleep(5)
+        time.sleep(5) #pauses program for 5 seconds
         
